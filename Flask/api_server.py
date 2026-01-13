8ea550bdd8fd21380f37462e6fd32df1d4f2b4e4
Alexander Guigni <alexander.guigni@outlook.com>
2026-01-12 20:20:03 -0600

    Requested changes

--- Flask/api_server.py
+++ Flask/api_server.py
@@ -1,69 +1,64 @@
 from flask import Flask, request
 from data import Data
-from task import Task, Fields
+from task import Task, Fields, TaskStatus
 
 app = Flask(__name__)
 
-@app.route("/getAllTasks", methods=["GET"])
+tasks_data = Data()
+
+@app.route("/tasks", methods=["GET"])
 def getAllTasks():
-    tasks_data = Data()
-    tasks = tasks_data.read_json()
-    return tasks
+    task_status = request.args.get("status")
+    
+    try:
+        if (task_status is not None):
+            if task_status not in [status.value for status in TaskStatus]:
+                raise ValueError("Invalid status value.")
+            tasks = tasks_data.get_tasks_by_status(task_status)
+            return tasks
+        else:
+            tasks = tasks_data.read_json()
+            return tasks
+    except ValueError as ex:
+        return {"error": str(ex)}, 400
+    except Exception as ex:
+        return {"error": "An unexpected error occurred.", "details": str(ex)}, 500
 
-@app.route("/getTaskByStatus/<task_status>", methods=["GET"])
-def getTaskByStatus(task_status):
-    tasks_data = Data()
-    tasks = tasks_data.get_tasks_by_status(task_status)
-    return tasks
-
-@app.route("/getTaskById/<task_id>", methods=["GET"])
-def getTaskById(task_id):
-    tasks_data = Data()
-    task = tasks_data.get_task_by_id(int(task_id))
-    return task
-
-@app.route("/create", methods=["POST"])
+@app.route("/tasks", methods=["POST"])
 def create():
     try:
-        if Fields.TITLE.value not in request.json or Fields.DESCRIPTION.value not in request.json or Fields.STATUS.value not in request.json:
-            raise ValueError("Missing required fields.")
         new_task = Task.from_dict(request.json)
-        tasks_data = Data()
         tasks_data.add_task(new_task.get_task_as_dict())
         return {"message": "Task created successfully.", "task": tasks_data.get_last_task()}, 201
     except ValueError as ex:
         return {"error": str(ex)}, 400
     except Exception as ex:
-        return {"error": "An unexpected error occurred."}, 500
+        return {"error": "An unexpected error occurred.", "details": str(ex)}, 500
 
 
-@app.route("/edit/<id>", methods=["PUT"])
+@app.route("/tasks/id/<id>", methods=["PUT"])
 def edit(id):
     try:
-        if Fields.TITLE.value not in request.json or Fields.DESCRIPTION.value not in request.json or Fields.STATUS.value not in request.json:
-                raise ValueError("Missing required fields.")
-        task_data = Data()
         updated_task = Task.from_dict(request.json).get_task_as_dict()
-        task = task_data.update_task(int(id), updated_task)
+        task = tasks_data.update_task(int(id), updated_task)
         if task is None:
             return {"error": "Task not found."}, 404
         return {"message": "Task updated successfully.", "task": task}, 200
     except ValueError as ex:
         return {"error": str(ex)}, 400
     except Exception as ex:
-        return {"error": "An unexpected error occurred."}, 500
+        return {"error": "An unexpected error occurred.", "details": str(ex)}, 500
 
 
-@app.route("/delete/<id>", methods=["DELETE"])
+@app.route("/tasks/id/<id>", methods=["DELETE"])
 def delete(id):
     try:
-        task_data = Data()
-        task = task_data.delete_task(int(id))
+        task = tasks_data.delete_task(int(id))
         if task is None:
             return {"error": "Task not found."}, 404
         return {"message": "Task deleted successfully.", "task": task}, 200
     except Exception as ex:
-        return {"error": "An unexpected error occurred."}, 500
+        return {"error": "An unexpected error occurred.", "details": str(ex)}, 500
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
